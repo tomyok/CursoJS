3fa82fc0a49515e1ed89f5c33eb3da6f0458dbee
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2024-11-09 17:37:04 -0300

    Conociendo las ventanas de js.

---
+++ ventanas.js
@@ -0,0 +1,11 @@
+'use strict'
+//Ventanas
+//Alertas
+alert('Hola');
+//Confirmacion
+var confir = confirm('Estas seguro?');
+console.log(confirm);
+//Ingreso de datos
+//Prompt siempre devuelve un string
+var res = parseInt(prompt('Que edad tenes?'));
+console.log(res);
